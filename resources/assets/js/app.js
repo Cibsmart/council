76327bb5d6fa86a0ea10426577e6646ee8a77b16
Barnabas Ifebude <barnabas.ifebude@tenece.com>
2017-12-07 02:57:16 +0100

    Completed episode 29: Added a Vue Flash Component

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -14,6 +14,7 @@ require('./bootstrap');
  */
 
 Vue.component('example', require('./components/Example.vue'));
+Vue.component('flash', require('./components/Flash.vue'));
 
 const app = new Vue({
     el: '#app'
