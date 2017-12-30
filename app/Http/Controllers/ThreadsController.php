3e97a9f8e1f5b447e41cb1d2ebcda95a86beafb9
Barnabas Ifebude <cibsmarte@gmail.com>
2017-12-30 14:34:54 +0100

    Completed episode 62: Tweaked the UI and modified failing test

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -136,7 +136,7 @@ class ThreadsController extends Controller
             $threads->where('channel_id', $channel->id);
         }
 
-        $threads = $threads->get();
+        $threads = $threads->paginate(25);
 
         return $threads;
     }
--- tests/Feature/ReadThreadTest.php
+++ tests/Feature/ReadThreadTest.php
@@ -105,7 +105,7 @@ class ReadThreadTest extends TestCase
 
         $response = $this->getJson(route('threads.index', ['popular' => 1]))->json();
 
-        $this->assertEquals([3,2,0], array_column($response, 'replies_count'));
+        $this->assertEquals([3,2,0], array_column($response['data'], 'replies_count'));
     }
 
 
@@ -123,7 +123,7 @@ class ReadThreadTest extends TestCase
 
         $response = $this->getJson(route('threads.index', ['unanswered' => 1]))->json();
 
-        $this->assertCount(1, $response);
+        $this->assertCount(1, $response['data']);
     }
 
 
