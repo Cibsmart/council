aa2f26a100c5b647f14f46134f3afce4c4bf5530
Barnabas Ifebude <cibsmarte@gmail.com>
2018-02-06 13:00:56 +0100

    Completed Episode 66: Trending-With-Redis

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -3,10 +3,28 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-8">
                 @include('threads._list')
                 
                 {{ $threads->render() }}
+            </div>
+            
+            <div class="col-md-4">
+                @if(count($trending))
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Trending Threads
+                        </div>
+                        
+                        <div class="panel-body">
+                            @foreach($trending as $thread)
+                                <li class="list-group-item">
+                                    <a href="{{ url($thread->path) }}">{{  $thread->title }} </a>
+                                </li>
+                            @endforeach
+                        </div>
+                    </div>
+                @endif
             </div>
         </div>
     </div>
