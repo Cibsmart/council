14646364cc8db8aebabcec0e398184f2c295b895
Barnabas Ifebude <cibsmarte@gmail.com>
2018-03-13 18:37:20 +0100

    Completed Episode 98: First Class Search Forum Integration

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -9,16 +9,12 @@ class SearchController extends Controller
 {
     public function show(Trending $trending)
     {
-        $search = request('q');
-
-        $threads = Thread::search($search)->paginate(25);
-
         if (request()->expectsJson()) {
-            return $threads;
+            return Thread::search(request('q'))->paginate(25);
         }
 
         $trending = $trending->get();
 
-        return view('threads.index', compact('threads', 'trending'));
+        return view('threads.search', compact('trending'));
     }
 }
