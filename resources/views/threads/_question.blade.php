510526f9921cc52effe7fe0f84749e61ffca5ce9
Barnabas Ifebude <cibsmarte@gmail.com>
2018-03-13 11:37:27 +0100

    Completed Episode 93: A Thread Can be Updated Ajax Request

--- resources/views/threads/_question.blade.php
+++ resources/views/threads/_question.blade.php
@@ -2,23 +2,21 @@
 <div class="panel panel-default" v-if="editing">
     <div class="panel-heading">
         <div class="level">
-            <input class="form-control" type="text" value="{{ $thread->title }}" >
+            <input class="form-control" type="text" v-model="form.title">
 
         </div>
     </div>
     
     <div class="panel-body">
         <div class="form-group">
-            <textarea class="form-control"  rows="10">
-                {{ $thread->body }}
-            </textarea>
+            <textarea class="form-control"  rows="10" v-model="form.body"></textarea>
         </div>
     </div>
     
     <div class="panel-footer">
         <div class="level">
-            <button class="btn btn-xs btn-primary level-item" @click="">Update</button>
-            <button class="btn btn-xs level-item" @click="editing = false">Cancel</button>
+            <button class="btn btn-xs btn-primary level-item" @click="update">Update</button>
+            <button class="btn btn-xs level-item" @click="resetForm">Cancel</button>
         
             @can('update', $thread)
                 <form action="{{ $thread->path() }}" method="post" class="ml-a">
@@ -41,17 +39,15 @@
                 <a href="{{ route('profiles.show', $thread->creator) }}">
                     {{ $thread->creator->name }}
                 </a>
-                posted {{ $thread->title }}
+                posted <span v-text="title"></span>
             </span>
 
         </div>
     </div>
     
-    <div class="panel-body">
-        {{ $thread->body }}
-    </div>
+    <div class="panel-body" v-text="body"></div>
     
-    <div class="panel-footer">
+    <div class="panel-footer" v-if="authorize('owns', thread)">
         <button class="btn btn-xs" @click="editing = true">Edit</button>
     </div>
 </div>
